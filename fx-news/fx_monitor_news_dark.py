fb2fcc7a9ad2f24ed8c6b7228c362e52fea177a1
frederictremblay <tremblayf@lmax.com>
2025-02-27 02:50:19 +0000

    remove style.css for now as causing issue in streamlit cloud

--- fx-news/fx_monitor_news_dark.py
+++ fx-news/fx_monitor_news_dark.py
@@ -570,6 +570,6 @@ with col4:
         st.info("No news items match your filters")
 
 
-# Include the CSS file
-with open("styles.css") as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# # Include the CSS file
+# with open("styles.css") as f:
+#     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
